33de07cc5e854993c2b6065b57f51ad137bb71b7
Ivan Borba <mrborbaivan@gmail.com>
2021-09-27 11:26:00 -0300

    feature/base: created a basic JSON-Server template

---
+++ server.js
@@ -0,0 +1,23 @@
+const jsonServer = require("json-server");
+const auth = require("json-server-auth");
+const cors = require("cors");
+const port = process.env.PORT || 3001;
+
+const app = jsonServer.create();
+const router = jsonServer.router("db.json");
+
+app.db = router.db;
+
+const rules = auth.rewriter({
+  users: 600,
+});
+
+app.use(cors());
+app.use(rules);
+app.use(auth);
+app.use(router);
+app.listen(port);
+
+console.log("Server is running on port:", port);
+
+/* A senha do Kenzinho é 123456 */
