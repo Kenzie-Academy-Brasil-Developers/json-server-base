ee729244010fc37adc65cfb5bc93b8ea984fbc0c
toledomg <toledomg@yahoo.com.br>
2023-03-19 12:57:37 -0300

    correção de arquivos

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const jsonServer = require("json-server");
 const auth = require("json-server-auth");
-import cors from "cors";
+const cors = require("cors");
 const port = process.env.PORT || 3001;
 
 const app = jsonServer.create();
