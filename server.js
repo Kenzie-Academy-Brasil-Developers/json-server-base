4adb32df9ae28abe0df11c8dd6cdf68e537dc765
tsunodajapa <gabrieltsunoda@gmail.com>
2023-04-23 21:05:36 -0300

    feat: ornganiza server.js

--- server.js
+++ server.js
@@ -2,15 +2,14 @@ const jsonServer = require("json-server");
 const auth = require("json-server-auth");
 const cors = require("cors");
 const port = process.env.PORT || 3001;
+const routes = require('./routes.json');
 
 const app = jsonServer.create();
 const router = jsonServer.router("db.json");
 
 app.db = router.db;
 
-const rules = auth.rewriter({
-  users: 600,
-});
+const rules = auth.rewriter(routes);
 
 app.use(cors());
 app.use(rules);
